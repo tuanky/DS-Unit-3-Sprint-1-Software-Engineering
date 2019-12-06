e31cbf34a5779f97ebdf7d7a35c42ce21336e342
tuanky <tky@terpmail.umd.edu>
2019-12-06 13:57:06 -0500

    add answers

---
+++ acme_report.py
@@ -0,0 +1,12 @@
+import numpy as np 
+from acme import Product
+def generate_products(num_products = 30):
+    products = []
+    name = ['Awesome', 'Shiny', 'Impressive',
+           'Portable', 'Improved']
+
+def inventory_report():
+    pass 
+
+if __name__ == '__main__':
+    inventory_report(generate_products())
