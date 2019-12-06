9feaa1240a90219f641fc88e5dedef3b5a4f2939
tuanky <tky@terpmail.umd.edu>
2019-12-06 13:28:32 -0500

    part 3

--- acme.py
+++ acme.py
@@ -3,7 +3,8 @@ import numpy as np
 class Product():
     """product class with default attribute: """
   
-    def __init__(self, name, price=10, weight=20, flammability=0.5, identifier=np.random.randint(1000000, 9999999)):
+    def __init__(self, name, price=10, weight=20, flammability=0.5, 
+        identifier=np.random.randint(1000000, 9999999)):
  
 
         self.name = name
@@ -17,20 +18,47 @@ class Product():
     def stealability(self):
     
        x = self.price/self.weight
+
        if x < 0.5:
-           return print("Not So stealable...")
+            print("Not So stealable...")
        elif (x >= 0.5 and x < 1.0):
-           return print("Kinda Stealable")
+            print("Kinda Stealable")
        else:
-           return print("Very stealable!") 
+            print("Very stealable!") 
 
     def explode(self):
-    
+        
         y = self.flammability * self.weight
         
         if y < 10: 
-            return print("...fizzle")
+            print("...fizzle")
         elif (y >= 10  and y < 50):
-            return print("...boom!")
+             print("...boom!")
         else:
-            return print("...BABOOM!!")
+             print("...BABOOM!!")
+
+class BoxingGlove(Product):
+
+    def __init__(self, name, price=10, weight=10, flammability=0.5, 
+        identifier=np.random.randint(1000000, 9999999)):
+ 
+
+        self.name = name
+        self.price = 10
+        self.weight = 10
+        self.flammability = 0.5
+        self.identifier = np.random.randint(1000000, 9999999)
+
+        pass
+
+    def explode(self):
+        print("...it's a glove")
+
+    def punch(self):
+        z = self.weight
+        if z < 5: 
+            print("That tickles")
+        elif (z >= 5  and z < 15):
+             print("Hey that hurt!")
+        else:
+             print("OUCH!")
